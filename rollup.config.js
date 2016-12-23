c962af88023202741f2cc761af716b16d622566b
Philippe Kalitine <philippe.kalitine@gmail.com>
2016-12-23 15:53:30 +0100

    build(rollup): simplify rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
-'use strict'
+import babel from 'rollup-plugin-babel'
+import filesize from 'rollup-plugin-filesize'
 
-const rollup = require('rollup')
-const babel = require('rollup-plugin-babel')
-const filesize = require('rollup-plugin-filesize')
-
-rollup.rollup({
+export default {
   entry: 'src/index.js',
+  dest: 'server.js',
+  format: 'iife',
+  banner: '#!/usr/bin/env node',
   plugins: [
     babel({
       exclude: 'node_modules/**',
@@ -13,10 +13,4 @@ rollup.rollup({
     }),
     filesize()
   ]
-}).then((bundle) => {
-  console.log('server.js: ')
-  bundle.write({
-    format: 'iife',
-    banner: '#!/usr/bin/env node',
-    dest: 'server.js'})
-})
+}
