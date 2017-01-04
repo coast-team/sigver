90305e028f682fa54701480ed2ccadbd1ca737c7
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-01-04 10:55:56 +0100

    chore(version): fix version for --help option

--- server.js
+++ server.js
@@ -282,7 +282,7 @@ let host = process.env.NODE_IP || '0.0.0.0';
 let port = process.env.NODE_PORT || 8000;
 
 program
-  .version('8.0.1', '-v, --version')
+  .version('8.0.2', '-v, --version')
   .option('-h, --host <n>', 'specify host (DEFAULT: process.env.NODE_IP || "localhost")')
   .option('-p, --port <n>', 'specify port (DEFAULT: process.env.NODE_PORT || 8000)')
   .on('--help', () => {
