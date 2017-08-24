51db77ad2ec49786556057911ee8932f559169c7
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-08-24 18:01:06 +0200

    test(karma): change Chrome to ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -69,7 +69,7 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['Chrome', 'Firefox'],
 
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['ChromeHeadless', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
